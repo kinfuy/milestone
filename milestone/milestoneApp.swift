39f3a019c57ecdc148b0818709b06182cf126636
kinfuy <yonghu520@outlook.com>
2023-09-07 22:36:06 +0800

    Initial Commit

---
+++ milestone/milestoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  milestoneApp.swift
+//  milestone
+//
+//  Created by 杨杨杨 on 2023/9/7.
+//
+
+import SwiftUI
+
+@main
+struct milestoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
